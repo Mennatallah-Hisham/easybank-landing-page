a3dd77528fe52b15832840ecbdd6c3f2e15db3e6
unknown <mennahisham1d@gmail.com>
2022-10-20 09:24:57 +0200

    mobile nav changes

--- script.js
+++ script.js
@@ -3,8 +3,10 @@
 const navBtn =document.querySelector(".btn-mobile-nav");
 
 const headerEl =document.querySelector(".header");
+const htmlEl =document.querySelector("html");
 
 navBtn.addEventListener("click",()=>{
-    console.log("clicked");
+   
     headerEl.classList.toggle("nav-open");
+    htmlEl.classList.toggle("hide-overflowY");
 })
